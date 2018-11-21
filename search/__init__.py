8ab41adc109a276d59d13c76b0de3462274004a6
ramses132 <yugo132@gmail.com>
2018-11-21 14:11:27 -0500

    [ADD] - dev - base

--- search/__init__.py
+++ search/__init__.py
@@ -6,14 +6,13 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 
-def create_app(test_config=None):
+def create_app(config_class=Config):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
             SECRET_KEY='dev'
             DATABASE=os.path.join(app.instance_path, 'search.sqlite',)
             )
-    if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
