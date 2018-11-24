3294cae9fb254e0276fd34420212afc839bc6485
ramses132 <yugo132@gmail.com>
2018-11-24 17:22:32 -0500

    [ADD] - base - search

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,20 @@
 import os
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
+moment = Moment()
+mail = Mail()
 
 
 def create_app(config_name):
     # create and configure the app
-
     app = Flask(__name__)
 
     # load the instance config, if it exists, when not testing
@@ -21,6 +26,9 @@ def create_app(config_name):
         pass
         
     db.init_app(app)
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    mail.init_app(app)
 
     if app.config['SSL_REDIRECT']:
         from flask_sslify import SSLify
