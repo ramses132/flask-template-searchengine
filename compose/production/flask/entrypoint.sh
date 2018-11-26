060abcffe3a1610a350ba44d8c1448d9cb3c6c70
ramses132 <yugo132@gmail.com>
2018-11-26 10:48:29 -0500

    [ADD] - base - search

--- compose/production/flask/entrypoint.sh
+++ compose/production/flask/entrypoint.sh
@@ -1,8 +1,36 @@
 #!/bin/sh
 set -e
 
+postgres_connection() {
+python << END
+import sys
+import psycopg2
+
+try:
+    psycopg2.connect(
+        dbname="${POSTGRES_DB}",
+        user="${POSTGRES_USER}",
+        password="${POSTGRES_PASSWORD}",
+        host="postgres"
+    )
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+if [[  -z "$POSTGRES_DB" ]]; then
+    echo "Running without postgres enviroment..."
+else
+    export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/${POSTGRES_DB}"
+    while ! [[ postgres_connection ]]; do
+        >&2 echo Postgres connection failed, retrying in 5 secs...
+        sleep 5
+    done
+fi
+
 while true; do
-    ./venv/bin/python flask deploy
+    flask deploy
     if [[ "$?" == "0" ]]; then
         break
     fi
